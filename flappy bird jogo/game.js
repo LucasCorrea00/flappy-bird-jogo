dd298219a2c7414666dcc28ce0db1519904d98e6
Lucasntc <lucas-correa18@hotmail.com>
2025-08-02 16:09:19 -0300

    game.js

--- flappy bird jogo/game.js
+++ flappy bird jogo/game.js
@@ -6,24 +6,24 @@ const gameContainer = document.getElementById('game-container');
 const flappyImg = new Image();
 flappyImg.src = 'assets/flappy_dunk.png';
 
-//Game constants
+
 const FLAP_SPEED = -5;
 const BIRD_WIDTH = 40;
 const BIRD_HEIGHT = 30;
 const PIPE_WIDTH = 50;
 const PIPE_GAP = 125;
 
-// Bird variables
+
 let birdX = 50;
 let birdY = 50;
 let birdVelocity = 0;
 let birdAcceleration = 0.1;
 
-// Pipe variables
+
 let pipeX = 400;
 let pipeY = canvas.height - 200;
 
-// score and highscore variables
+
 let scoreDiv = document.getElementById('score-display');
 let score = 0;
 let highScore = 0;
